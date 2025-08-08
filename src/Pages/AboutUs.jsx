ed24338f19ada05afccdeca5e1ee3e44065d337d
tasnim29 <tasnimmahmudnjr@gmail.com>
2025-08-09 00:46:42 +0600

    about us done

--- src/Pages/AboutUs.jsx
+++ src/Pages/AboutUs.jsx
@@ -1,60 +1,79 @@
-import React from "react";
+import React, { use } from "react";
+import CountUp from "react-countup";
+import { AuthContext } from "../Context/AuthContext";
 
 const AboutUs = () => {
+  const { theme } = use(AuthContext);
+
+  const isDark = theme === "dark";
   return (
-    <div className="py-36">
-      <div className=" ">
+    <div
+      className={`py-36 ${
+        isDark ? "bg-gray-900 text-gray-100" : "bg-white text-gray-900"
+      }`}
+    >
+      <div>
         {/* first section */}
-        <div className=" sm:text-center ">
+        <div className="sm:text-center">
           {/* heading part */}
           <div className="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
-            <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto">
+            <h2
+              className={`max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight ${
+                isDark ? "text-gray-100" : "text-gray-900"
+              } sm:text-4xl md:mx-auto`}
+            >
               <span className="relative inline-block">
                 <svg
                   viewBox="0 0 52 24"
                   fill="currentColor"
-                  className="absolute top-0 left-0 z-0 hidden w-32 -mt-8 -ml-20 text-blue-gray-100 lg:w-32 lg:-ml-28 lg:-mt-10 sm:block"
+                  className={`absolute top-0 left-0 z-0 hidden w-32 -mt-8 -ml-20 ${
+                    isDark ? "text-blue-gray-700" : "text-blue-gray-100"
+                  } lg:w-32 lg:-ml-28 lg:-mt-10 sm:block`}
                 >
                   <defs>
-                    <pattern
-                      id="5dc90b42-5ed4-45a6-8e63-2d78ca9d3d95"
-                      x="0"
-                      y="0"
-                      width=".135"
-                      height=".30"
-                    >
+                    <pattern id="pattern" x="0" y="0" width=".135" height=".30">
                       <circle cx="1" cy="1" r=".7" />
                     </pattern>
                   </defs>
-                  <rect
-                    fill="url(#5dc90b42-5ed4-45a6-8e63-2d78ca9d3d95)"
-                    width="52"
-                    height="24"
-                  />
+                  <rect fill="url(#pattern)" width="52" height="24" />
                 </svg>
                 <span className="relative">The</span>
               </span>{" "}
               Hello! This is Echo Journal
             </h2>
-            <p className="text-base text-gray-700 md:text-lg">
+            <p
+              className={`${
+                isDark ? "text-gray-300" : "text-gray-700"
+              } text-base md:text-lg`}
+            >
               A company turning ideas into beautiful things
             </p>
           </div>
           <div className="mb-4 transition-shadow duration-300 hover:shadow-xl lg:mb-6">
             <img
-              className="object-cover w-full h-[500px] rounded shadow-lg "
+              className="object-cover w-full h-[500px] rounded shadow-lg"
               src="https://i.ibb.co.com/pr01skHn/CPFy0-Yo-Wc-AAC1-Yw.jpg"
               alt=""
             />
           </div>
         </div>
+
         {/* second section */}
         <div className="px-4 py-16 max-w-7xl mx-auto">
           <div className="flex flex-col-reverse lg:flex-row justify-between gap-10 items-center">
             {/* Left Text Content */}
             <div className="flex flex-col justify-center md:pr-8 xl:pr-0 lg:max-w-lg text-center lg:text-left">
-              <div className="flex items-center justify-center lg:justify-start w-16 h-16 mb-4 mx-auto lg:mx-0 rounded-full bg-teal-100">
-                <svg className="text-teal-900 w-7 h-7" viewBox="0 0 24 24">
+              <div
+                className={`flex items-center justify-center lg:justify-start w-16 h-16 mb-4 mx-auto lg:mx-0 rounded-full ${
+                  isDark ? "bg-teal-900" : "bg-teal-100"
+                }`}
+              >
+                <svg
+                  className={`${
+                    isDark ? "text-teal-300" : "text-teal-900"
+                  } w-7 h-7`}
+                  viewBox="0 0 24 24"
+                >
                   <polyline
                     fill="none"
                     stroke="currentColor"
@@ -97,10 +116,18 @@ const AboutUs = () => {
                 </svg>
               </div>
               <div className="max-w-xl mb-6 mx-auto lg:mx-0">
-                <h2 className="text-3xl sm:text-4xl font-bold text-gray-900 mb-4">
+                <h2
+                  className={`text-3xl sm:text-4xl font-bold mb-4 ${
+                    isDark ? "text-gray-100" : "text-gray-900"
+                  }`}
+                >
                   Who Are We?
                 </h2>
-                <p className="text-base text-gray-700 md:text-lg">
+                <p
+                  className={`text-base md:text-lg ${
+                    isDark ? "text-gray-300" : "text-gray-700"
+                  }`}
+                >
                   We are a passionate team of content creators, developers, and
                   enthusiasts who believe in sharing knowledge, ideas, and
                   stories through blogs. Whether it’s the latest trends in tech
@@ -111,7 +138,11 @@ const AboutUs = () => {
               <div>
                 <a
                   href="/"
-                  className="inline-flex items-center font-semibold text-indigo-600 hover:text-indigo-800 transition-colors duration-200"
+                  className={`inline-flex items-center font-semibold transition-colors duration-200 ${
+                    isDark
+                      ? "text-indigo-400 hover:text-indigo-600"
+                      : "text-indigo-600 hover:text-indigo-800"
+                  }`}
                 >
                   Learn more
                   <svg
@@ -140,93 +171,143 @@ const AboutUs = () => {
             </div>
           </div>
         </div>
+
         {/* third section */}
-        <div className="  bg-[#EDF2FC]">
-          <div className="grid gap-10 row-gap-8 lg:grid-cols-3 max-w-7xl mx-auto px-4 py-16">
-            <div>
-              <div className="flex">
-                <h6 className="mr-2 text-4xl font-bold md:text-5xl text-deep-purple-accent-400">
-                  86K
-                </h6>
-                <div className="flex items-center justify-center rounded-full bg-teal-accent-400 w-7 h-7">
-                  <svg
-                    className="text-teal-900 w-7 h-7"
-                    stroke="currentColor"
-                    viewBox="0 0 52 52"
-                  >
-                    <polygon
-                      strokeWidth="3"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      fill="none"
-                      points="29 13 14 29 25 29 23 39 38 23 27 23"
-                    />
-                  </svg>
-                </div>
+        <div
+          className={`${
+            isDark ? "bg-gray-800" : "bg-[#EDF2FC]"
+          } max-w-7xl mx-auto px-4 py-16 grid gap-10 row-gap-8 lg:grid-cols-3`}
+        >
+          <div>
+            <div className="flex">
+              <h6
+                className={`mr-2 text-4xl font-bold md:text-5xl text-deep-purple-accent-400 ${
+                  isDark ? "text-purple-400" : ""
+                }`}
+              >
+                <CountUp end={25} duration={20} />
+              </h6>
+              <div
+                className={`flex items-center justify-center rounded-full ${
+                  isDark ? "bg-teal-700" : "bg-teal-accent-400"
+                } w-7 h-7`}
+              >
+                <svg
+                  className={`w-7 h-7 ${
+                    isDark ? "text-teal-300" : "text-teal-900"
+                  }`}
+                  stroke="currentColor"
+                  viewBox="0 0 52 52"
+                >
+                  <polygon
+                    strokeWidth="3"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    fill="none"
+                    points="29 13 14 29 25 29 23 39 38 23 27 23"
+                  />
+                </svg>
               </div>
-              <p className="mb-2 font-bold md:text-lg">Downloads</p>
-              <p className="text-gray-700">
-                Three movie stars, Chloe, Lexa, and Jon, are filming a movie in
-                the Amazon. They’re very famous and very high-maintenance
-                effort.
-              </p>
             </div>
-            <div>
-              <div className="flex">
-                <h6 className="mr-2 text-4xl font-bold md:text-5xl text-deep-purple-accent-400">
-                  1.3K
-                </h6>
-                <div className="flex items-center justify-center rounded-full bg-teal-accent-400 w-7 h-7">
-                  <svg
-                    className="text-teal-900 w-7 h-7"
-                    stroke="currentColor"
-                    viewBox="0 0 52 52"
-                  >
-                    <polygon
-                      strokeWidth="3"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      fill="none"
-                      points="29 13 14 29 25 29 23 39 38 23 27 23"
-                    />
-                  </svg>
-                </div>
+            <p
+              className={`mb-2 font-bold md:text-lg ${
+                isDark ? "text-gray-100" : "text-gray-900"
+              }`}
+            >
+              Blogs Published
+            </p>
+            <p className={`${isDark ? "text-gray-300" : "text-gray-700"}`}>
+              From the latest in tech to exciting sports updates, explore a
+              growing collection of insightful articles.
+            </p>
+          </div>
+
+          <div>
+            <div className="flex">
+              <h6
+                className={`mr-2 text-4xl font-bold md:text-5xl text-deep-purple-accent-400 ${
+                  isDark ? "text-purple-400" : ""
+                }`}
+              >
+                <CountUp end={300} duration={20} />
+              </h6>
+              <div
+                className={`flex items-center justify-center rounded-full ${
+                  isDark ? "bg-teal-700" : "bg-teal-accent-400"
+                } w-7 h-7`}
+              >
+                <svg
+                  className={`w-7 h-7 ${
+                    isDark ? "text-teal-300" : "text-teal-900"
+                  }`}
+                  stroke="currentColor"
+                  viewBox="0 0 52 52"
+                >
+                  <polygon
+                    strokeWidth="3"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    fill="none"
+                    points="29 13 14 29 25 29 23 39 38 23 27 23"
+                  />
+                </svg>
               </div>
-              <p className="mb-2 font-bold md:text-lg">Subscribers</p>
-              <p className="text-gray-700">
-                One day, after filming a scene deep in the rainforest, the three
-                actors and their agents decide to head back to home base by
-                foot.
-              </p>
             </div>
-            <div>
-              <div className="flex">
-                <h6 className="mr-2 text-4xl font-bold md:text-5xl text-deep-purple-accent-400">
-                  52M
-                </h6>
-                <div className="flex items-center justify-center rounded-full bg-teal-accent-400 w-7 h-7">
-                  <svg
-                    className="text-teal-900 w-7 h-7"
-                    stroke="currentColor"
-                    viewBox="0 0 52 52"
-                  >
-                    <polygon
-                      strokeWidth="3"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      fill="none"
-                      points="29 13 14 29 25 29 23 39 38 23 27 23"
-                    />
-                  </svg>
-                </div>
+            <p
+              className={`mb-2 font-bold md:text-lg ${
+                isDark ? "text-gray-100" : "text-gray-900"
+              }`}
+            >
+              Subscribers
+            </p>
+            <p className={`${isDark ? "text-gray-300" : "text-gray-700"}`}>
+              Join our community of tech enthusiasts and sports fans receiving
+              weekly insights and trending stories.
+            </p>
+          </div>
+
+          <div>
+            <div className="flex">
+              <h6
+                className={`mr-2 text-4xl font-bold md:text-5xl text-deep-purple-accent-400 ${
+                  isDark ? "text-purple-400" : ""
+                }`}
+              >
+                <CountUp end={20} duration={20} />
+              </h6>
+              <div
+                className={`flex items-center justify-center rounded-full ${
+                  isDark ? "bg-teal-700" : "bg-teal-accent-400"
+                } w-7 h-7`}
+              >
+                <svg
+                  className={`w-7 h-7 ${
+                    isDark ? "text-teal-300" : "text-teal-900"
+                  }`}
+                  stroke="currentColor"
+                  viewBox="0 0 52 52"
+                >
+                  <polygon
+                    strokeWidth="3"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    fill="none"
+                    points="29 13 14 29 25 29 23 39 38 23 27 23"
+                  />
+                </svg>
               </div>
-              <p className="mb-2 font-bold md:text-lg">Cookies</p>
-              <p className="text-gray-700">
-                Suddenly, they come to a large river. On the riverbank, they
-                find a small rowboat, but it’s only big enough to hold two of
-                them at one time.
-              </p>
             </div>
+            <p
+              className={`mb-2 font-bold md:text-lg ${
+                isDark ? "text-gray-100" : "text-gray-900"
+              }`}
+            >
+              Categories
+            </p>
+            <p className={`${isDark ? "text-gray-300" : "text-gray-700"}`}>
+              Explore a variety of categories including AI, gadgets, football,
+              basketball, coding, and more.
+            </p>
           </div>
         </div>
       </div>
